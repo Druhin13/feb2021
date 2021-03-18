9f8cad89b18fa155971ad660c58094b16ed9457b
Druhin Tarafder <druhinpro@gmail.com>
2021-03-18 22:45:42 +0530

    Add files via upload

---
+++ redirect.js
@@ -0,0 +1,35 @@
+function StamuraiRedirect() {
+	setTimeout(function () {
+		window.location = "https://druh.in/stamurai";
+	}, 3000);
+}
+
+function InsaneDealsRedirect() {
+	setTimeout(function () {
+		window.location = "https://druh.in/insanedeals";
+	}, 3000);
+}
+
+function FathomOpticsRedirect() {
+	setTimeout(function () {
+		window.location = "https://druh.in/fathomoptics";
+	}, 3000);
+}
+
+function KeralaOnRoadRedirect() {
+	setTimeout(function () {
+		window.location = "https://druh.in/keralaonroad";
+	}, 3000);
+}
+
+function MausumiRedirect() {
+	setTimeout(function () {
+		window.location = "https://druh.in/mausumi";
+	}, 3000);
+}
+
+function KhatabookRedirect() {
+	setTimeout(function () {
+		window.location = "https://druh.in/khatabook";
+	}, 3000);
+}
